6724b4b761d8b08b67460d212ce870c531994286
Murat Eren Nar <muraterennar@hotmail.com>
2021-10-19 12:32:11 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concreate;
+using DataAccess.Concreate.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+            foreach (var item in productManager.GetAll())
+            {
+                Console.WriteLine(item.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concreate/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concreate.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _prodcuts;
+
+        public InMemoryProductDal()
+        {
+            _prodcuts = new List<Product>
+            {
+                new Product{ProductId=1,CategoryId=1, ProductName="Bardak",UnitPrice=15,UnitsInStock=20},
+                new Product{ProductId=2,CategoryId=2, ProductName="Kamera",UnitPrice=500,UnitsInStock=5},
+                new Product{ProductId=3,CategoryId=2, ProductName="Telefon",UnitPrice=2500,UnitsInStock=30},
+                new Product{ProductId=4,CategoryId=2, ProductName="Klavye",UnitPrice=50,UnitsInStock=10},
+                new Product{ProductId=4,CategoryId=2, ProductName="Mouse",UnitPrice=32,UnitsInStock=4}
+            };
+        }
+
+        public void Add(Product product)
+        {
+            _prodcuts.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete = _prodcuts.SingleOrDefault(p => p.ProductId == product.ProductId);
+
+            _prodcuts.Remove(productToDelete);
+        }
+
+        public List<Product> GetAll()
+        {
+            return _prodcuts;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _prodcuts.Where(p => p.CategoryId == categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _prodcuts.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+        }
+    }
+}
