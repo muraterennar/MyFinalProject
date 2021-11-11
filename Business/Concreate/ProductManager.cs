835d50ed0534685cf12a5d745f9483193b46ffec
Murat Eren Nar <muraterennar@hotmail.com>
2021-11-11 12:45:26 +0300

    11 Kasım 2021

--- Business/Concreate/ProductManager.cs
+++ Business/Concreate/ProductManager.cs
@@ -29,7 +29,7 @@ namespace Business.Concreate
             _categoryService = categoryService;
         }
 
-        //[ValidationAspect(typeof(ProductValidator))]
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             IResult result = BusinessRules.Run(
