59bba5cfaeedd97119ebe19041387963743f2424
Murat Eren Nar <muraterennar@hotmail.com>
2021-11-27 14:52:48 +0300

    27.11.2021 Commit 1

---
+++ Core/Entities/Security/JWT/JwtHelper.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities.Concreate;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Security.JWT
+{
+    public class JwtHelper : ITokenHelper
+    {
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+        private DateTime _accessTokenExpiration;
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenOptions = Configuration.GetSection("TokenOptions");
+
+        }
+        
+    }
+}
